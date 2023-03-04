d1eed63082e40f7cd1f76315bec8bd63f2c31d57
shivraj <sk40fs@gmail.com>
2023-03-04 17:37:06 +0530

    background-image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home(){
   return (
       <div className="container conatinerHome d-flex justify-content-center">
-          <img src="" style={{width:'20rem',height:'30rem'}} alt="" />         
+          <img src="https://media3.giphy.com/media/yZ9unuhuUemYvoeQaR/giphy.gif?cid=6c09b952ef94dab176f00018a5b403ff6a46471c5c346ed9&rid=giphy.gif&ct=s" style={{width:'20rem',height:'30rem'}} alt="" />         
     </div>
   )
 }
