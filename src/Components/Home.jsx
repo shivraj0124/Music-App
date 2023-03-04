5bd959677e375866f3aa5557b203f43851752efd
shivraj <sk40fs@gmail.com>
2023-03-04 17:20:33 +0530

    Remove Image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Home(){
   return (
       <div className="container conatinerHome d-flex justify-content-center">
-          <img src="https://media.tenor.com/D5W9K_bAGbIAAAAC/cute-music.gif" style={{width:'20rem',height:'30rem'}} alt="" />
-          
+          <img src="" style={{width:'20rem',height:'30rem'}} alt="" />         
     </div>
   )
 }
