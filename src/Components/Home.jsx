7f1829707ad6d9a308f5c0136cb5e37a5646556d
shivraj <sk40fs@gmail.com>
2023-03-04 17:05:17 +0530

    Image Added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Home(){
   return (
-      <div className="container d-flex justify-content-center">
-          <img src="https://t3.ftcdn.net/jpg/02/11/56/32/360_F_211563252_V0LoYP3RgLWEKghgKARZVsfA748qk0T6.jpg" style={{width:'20rem',height:'30rem'}} alt="" />
+      <div className="container conatinerHome d-flex justify-content-center">
+          <img src="https://media.tenor.com/D5W9K_bAGbIAAAAC/cute-music.gif" style={{width:'20rem',height:'30rem'}} alt="" />
+          
     </div>
   )
 }
